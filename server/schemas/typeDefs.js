14201ae122fa6f8557216ad4aa17acdded14036b
Emily Kersey <ekerseyc@gmail.com>
2022-05-02 13:50:48 -0400

    typeDefs updated

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,11 +2,52 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    role: String
+    firstName: String
+    lastName: String
+    createdAt: Integer
+    updatedAt: Integer
+    enabled: Boolean
+    ReviewId: ID
+    PostId: ID
   }
+
+  type Profile {
+    user: [_id!, role]
+    availability: Boolean
+    contactInfo: String
+    porfolioURL: String
+    description: String
+    review: [Review]
+    pricing: Integer
+    travelRange: Integer
+  }
+
+  type Review {
+    user: [_id!]
+    rating: Boolean
+    description: String
+    createdAt: Integer
+    updatedAt: Integer
+    enabled: Boolean
+  }
+
+  type HiringPost {
+    postId: ID
+    user: [_id!]
+    title: String
+    description: String
+    eventDate: Integer
+    createdAt: Integer
+    updatedAt: Integer
+    enabled: Boolean
+    contactForm: String
+    }
+
 
   type Auth {
     token: ID!
