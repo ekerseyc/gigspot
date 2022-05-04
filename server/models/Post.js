16cc1b2bd913e972aa3a6ec33ad6f827d68801f7
kevnviek303@yahoo.com <kevnivek303@yahoo.com>
2022-05-04 12:11:44 -0400

    updated js

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,9 +1,9 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const postSchema = new Schema({
   description: {
     type: String,
-    required: 'You must leave a description',
+    required: "You must leave a description",
     minlength: 1,
     maxlength: 280,
     trim: true,
@@ -15,6 +15,6 @@ const postSchema = new Schema({
   },
 });
 
-const Post = model('Post', postSchema);
+const Post = model("Post", postSchema);
 
 module.exports = Post;
