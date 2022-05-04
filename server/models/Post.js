9f9f78bc08458aff5e851a1c3e2c6ed5396e0f30
kevnviek303@yahoo.com <kevnivek303@yahoo.com>
2022-05-04 14:24:03 -0400

    updated js

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { Schema, model } = require("mongoose");
 
 const postSchema = new Schema({
@@ -14,49 +13,32 @@ const postSchema = new Schema({
     required: true,
     trim: true,
   },
-=======
-const {Schema, model} = require('mongoose');
-
-const postSchema = new Schema({
-    description: {
+  slug: String,
+  ratingsAverage: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+    default: 4.5,
+    min: [1, "Rating must be above 0"],
+    max: [5, "Rating must be below or equal 5.0"],
+    set: (value) => Math.round(value * 10) / 10,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  locations: [
+    {
+      type: {
         type: String,
-        required: 'You must leave a description',
-        minlength: 1,
-        maxlength: 280,
-        trim: true,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+      address: String,
+      day: Number,
     },
-    author: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    slug: String,
-    ratingsAverage: {
-        type: Number,
-        required: [true, "A tour must have a duration"],
-        default: 4.5,
-        min: [1, "Rating must be above 0"],
-        max: [5, "Rating must be below or equal 5.0"],
-        set: (value) => Math.round(value * 10) / 10,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-        select: false,
-    },
-    locations: [
-        {
-            type: {
-                type: String,
-                default: "Point",
-                enum: ["Point"],
-            },
-            coordinates: [Number],
-            address: String,
-            day: Number,
-        },
-    ],
->>>>>>> main
+  ],
 });
 
 const Post = model("Post", postSchema);
