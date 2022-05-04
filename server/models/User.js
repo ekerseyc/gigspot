694b9208fad1079c1f2e6866b390464f92be37f5
kevnviek303@yahoo.com <kevnivek303@yahoo.com>
2022-05-04 16:14:31 -0400

    updated models

--- server/models/User.js
+++ server/models/User.js
@@ -1,73 +1,89 @@
-const {Schema, model} = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+
+  location: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+  //   passwordConfirm: {
+  //     type: String,
+  //     required: [true, "Please confirm your password"],
+  //     validate: {
+  //       // This only work on save!
+  //       validator: function (el) {
+  //         return el === this.password;
+  //       },
+  //       message: "Passwords are not the same.",
+  //     },
+  //   },
+
+  photo: String,
+  role: {
+    type: String,
+    enum: ["user", "employer", "admin"],
+    default: "user",
+  },
+
+  Active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
     },
   ],
-     email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/, 'Must match an email address!'],
+
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
     },
-    password: {
-        type: String,
-        required: true,
-        minlength: 5,
-    },
-    passwordConfirm: {
-        type: String,
-        required: [true, "Please confirm your password"],
-        validate: {
-            // This only work on save!
-            validator: function (el) {
-                return el === this.password;
-            },
-            message: "Passwords are not the same.",
-        },
-    },
-    photo: String,
-    role: {
-        type: String,
-        enum: ["user", "employer", "admin"],
-        default: "user",
-    },
-    Active: {
-        type: Boolean,
-        default: true,
-        select: false,
-    },
-    posts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Post',
-        },
-    ],
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review',
-        }
-    ]
+  ],
 });
 
-userSchema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('password')) {
-        const saltRounds = 10;
-        this.password = await bcrypt.hash(this.password, saltRounds);
-    }
-    next();
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
 });
 
 userSchema.methods.isCorrectPassword = async function (password) {
-    return bcrypt.compare(password, this.password);
+  return bcrypt.compare(password, this.password);
 };
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
