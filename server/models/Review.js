7c5513b7b0a748b8431c657f64b95e7ee32bdf97
Carrington <hartis.c99@gmail.com>
2022-05-05 11:37:03 -0400

    stash

--- server/models/Review.js
+++ server/models/Review.js
@@ -34,10 +34,8 @@ const reviewSchema = new Schema(
   }
 );
 
-const Review = model("Review", reviewSchema);
 
-
-const Review = model('Review', reviewSchema)
+const Review = model('Review', reviewSchema);
 
 module.exports = Review;
 
