b11b2dd499a9628c821dda820f0f93ddc4e604d6
kevnviek303@yahoo.com <kevnivek303@yahoo.com>
2022-05-06 17:15:53 -0400

    updated js

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,43 +1,43 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { CREATE_POST } from '../utils/mutations';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { CREATE_POST } from "../utils/mutations";
+import styled from "styled-components";
 
 // import Auth from '../utils/auth';
 
 // styled const
 const PostWrapper = styled.div`
-display: flex;
-flex-wrap: wrap;
-justify-content: space-around;
-padding: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 10px;
 `;
 
 const PostDiv = styled.div`
-background: #F9F9F9;
-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-border-radius: 10px;
-width: 50%;
-height: 50%;
-padding: 10px;
+  background: #f9f9f9;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  width: 50%;
+  height: 50%;
+  padding: 10px;
 `;
 
 const H3 = styled.h3`
-text-align: center;
-padding-top: 10px;
+  text-align: center;
+  padding-top: 10px;
 `;
 
 const NameInput = styled.input`
-width: 100%;
+  width: 100%;
   padding: 12px;
   border: 1px solid #ccc;
   border-radius: 4px;
   resize: vertical;
-  `;
+`;
 
 const CatInput = styled.input`
-width: 100%;
+  width: 100%;
   padding: 12px;
   border: 1px solid #ccc;
   border-radius: 4px;
@@ -46,7 +46,7 @@ width: 100%;
 `;
 
 const DescInput = styled.input`
-width: 100%;
+  width: 100%;
   padding: 12px;
   border: 1px solid #ccc;
   border-radius: 4px;
@@ -55,14 +55,14 @@ width: 100%;
 `;
 
 const SubmitBtn = styled.button`
-background: #6EBEED;
- font-size: large;
- color: white;
- padding: 10px 20px;
- border: none;
- border-radius: 4px;
- margin-top: 10px;
- float: right;
+  background: #6ebeed;
+  font-size: large;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  margin-top: 10px;
+  float: right;
 `;
 
 const PostForm = () => {
@@ -110,46 +110,31 @@ const PostForm = () => {
       <H3>Post a gig</H3>
       <PostWrapper>
         <PostDiv>
-
-      <form onSubmit={handleFormSubmit}>
-        
-        <div>
-          <NameInput
-            placeholder="Name..."
-            name="author"
-            value={formState.author}
-            onChange={handleChange}
-            />
-            <CatInput
-              placeholder="Category..."
-              name="category"
-              value={formState.category}
-              onChange={handleChange}
+          <form onSubmit={handleFormSubmit}>
+            <div>
+              <NameInput
+                placeholder="Name..."
+                name="author"
+                value={formState.author}
+                onChange={handleChange}
               />
-          <DescInput
-            placeholder="Description (date, time, etc)..."
-            name="description"
-            value={formState.description}
-            onChange={handleChange}
-<<<<<<< HEAD
-          />
-          <input
-            placeholder="Category..."
-            name="category"
-            value={formState.category}
-            onChange={handleChange}
-          />
-          <button type="submit">Submit</button>
-=======
-            />
-          <SubmitBtn type="submit">
-            Submit
-          </SubmitBtn>
->>>>>>> main
-        </div>
-      </form>
-            </PostDiv>
-            </PostWrapper>
+              <CatInput
+                placeholder="Category..."
+                name="category"
+                value={formState.category}
+                onChange={handleChange}
+              />
+              <DescInput
+                placeholder="Description (date, time, etc)..."
+                name="description"
+                value={formState.description}
+                onChange={handleChange}
+              />
+              <SubmitBtn type="submit">Submit</SubmitBtn>
+            </div>
+          </form>
+        </PostDiv>
+      </PostWrapper>
     </main>
   );
 };
